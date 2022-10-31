bf0ba7b50cf5444aefb4513ea8bfb12dacefc32b
martha_ibarra <marthaibarra10@gmail.com>
2022-10-31 17:07:01 -0600

    Mejora en la validacion

--- validationFormSingle.js
+++ validationFormSingle.js
@@ -3,12 +3,16 @@ const buttonListener = document.querySelector("button");
 var chk = document.getElementById("flexRadioDefault1");
 chk.addEventListener("change", action, false);
 
+var chk2 = document.getElementById("flexRadioDefault2");
+chk2.addEventListener("change", action, false);
+
 function action(){
     const inputName = document.querySelector(".nombre");
     const inputApellido = document.querySelector(".apellido");
     var elemento = document.forms["fvalida"]["checkbox"].checked;
     //var elemento2 = document.forms["fvalida"]["flexRadioDefault"].checked;
     var elemento2 = chk.checked;
+    var elemento3 = chk2.checked;
     
     if(inputName.value.length==0){
         alert("Agregue el nombre");
@@ -22,7 +26,7 @@ function action(){
             document.inputApellido.focus();
             return false;
         }else{
-            if(elemento2 == false){
+            if(elemento2 == false && elemento3 == false){
                 alert("Atención le falta seleccionar el radiobutton");
             }else{
                 if(elemento == false){
